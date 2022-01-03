4d7ee1b471af59ffe631c0a2c9ebecd79cbc4d80
clintonjosephs <clintonjosephs26@gmail.com>
2022-01-03 18:06:35 +0100

    setup awesomebooks refactor_with_classes

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,4 +1,56 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-/* eslint-disable prefer-const */
-let booksData = [];
+class Books {
+    constructor (element, storedItems) {
+        this.showBooksElement = element;
+        this.booksData = storedItems;
+    }
+
+    updateStorage() {
+        localStorage.setItem('bookData', JSON.stringify(this.booksData));
+    }
+
+    static createBookDisplay (book, empty = false) {
+        const li = document.createElement('li');
+        if (!empty) {
+            li.innerHTML = `<h3> ${book.title} </h3>
+                            <p>by - ${book.author} </p>
+                            <input type="button" value="Remove" id="${book.id}" class="removeBook" onClick="removeBook('${book.id}')"/>`;
+        } else {
+            li.innerHTML = `<h3>No books available</h3>`;
+        }
+      
+        return li;
+    }
+
+    updateUI () {
+        while (this.showBooksElement.firstChild) {
+            this.showBooksElement.removeChild(this.showBooksElement.firstChild);
+        }
+
+        if (this.booksData.length > 0) {
+            for (const book of this.booksData) {
+                this.showBooksElement.appendChild(Books.createBookDisplay(book));
+            }
+        } else {
+            this.showBooksElement.appendChild(Books.createBookDisplay({}, true));
+        }   
+    }
+
+    add (title, author) {
+        const bookId = Math.random().toString(36).slice(2);
+        this.booksData.unshift({
+          id: bookId,
+          title: title,
+          author: author,
+        });
+        this.updateStorage();
+        this.updateUI();
+    }
+
+    remove (bookId) {
+        this.booksData = this.booksData.filter((books) => books.id !== bookId);
+        this.updateStorage();
+        this.updateUI();
+    }
+}
--- assets/js/hardworker.js
+++ assets/js/hardworker.js
@@ -1,77 +1,29 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
+const removeBtn = document.querySelectorAll('.removeBook');
 const titleInput = document.querySelector('#book_title');
 const authorInput = document.querySelector('#book_author');
 const addBookBtn = document.querySelector('#add_book');
 const addForm = document.querySelector('.addBookForm');
 const bookList = document.querySelector('.list_present');
+let localStorageData = JSON.parse(localStorage.getItem('bookData'));
+
+if (localStorageData.length === 0 || localStorageData === null) {
+    localStorageData = [];
+}
+const booksBinding = new Books(bookList, localStorageData);
 
 function addBook() {
-  const titleValue = titleInput.value;
-  const authorValue = authorInput.value;
-
-  if (titleValue !== '' && authorValue !== '') {
-    if (booksData.length === 0) {
-      bookList.innerHTML = '';
-    }
-
-    const bookId = Math.random().toString(36).slice(2);
-    booksData.unshift(
-      {
-        id: bookId,
-        title: titleValue,
-        author: authorValue,
-      },
-    );
-
-    localStorage.setItem('bookData', JSON.stringify(booksData));
+    const title = titleInput.value;
+    const author = authorInput.value;
+    booksBinding.add(title, author);
     addForm.reset();
+}
 
-    bookList.innerHTML = `<li>
-                            <h3> ${titleValue} </h3>
-                            <p>by - ${authorValue} </p>
-                            <input type="button" value="Remove" id="${bookId}" class="removeBook" onClick="removeBook('${bookId}')"/>
-                        </li>${bookList.innerHTML}`;
-  }
+function removeBook(bookId) {
+   booksBinding.remove(bookId);
 }
 
 addBookBtn.addEventListener('click', addBook);
 
-function buildBookLists(book) {
-  const bookData = `<li>
-                          <h3> ${book.title} </h3>
-                          <p>by - ${book.author} </p>
-                          <input type="button" value="Remove" id="${book.id}" onClick="removeBook('${book.id}')"/>
-                      </li>`;
-  return bookData;
-}
-
-function readBookData() {
-  let booksBuild = '';
-  if (booksData.length > 0) {
-    booksData.forEach((books) => {
-      booksBuild += buildBookLists(books);
-    });
-  } else {
-    booksBuild = '<li><h3>No books available yet!</h3></li>';
-  }
-
-  bookList.innerHTML = booksBuild;
-}
-
-function removeBook(bookId) {
-  booksData = booksData.filter((books) => books.id !== bookId);
-  localStorage.setItem('bookData', JSON.stringify(booksData));
-  readBookData();
-}
-// Fetch data from localstorage
-
-const localStorageData = JSON.parse(localStorage.getItem('bookData'));
-function getLocalStorageData() {
-  if (localStorageData !== null) {
-    booksData = localStorageData;
-    readBookData();
-  }
-}
-
-window.onresize = getLocalStorageData();
+window.onresize = booksBinding.updateUI();
